b1bffa5b501f3f1a3b66f38099ad899dc6494faf
Nazar Poshtarenko <nazarpo@softwareplanet.uk.com>
2018-07-27 11:07:58 +0300

    initial commit before deploying

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const router = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const DB_URL = 'mongodb://localhost/interlink-meetup';
+const DB_URL = 'mongodb://nazarposhtarenko:Tb37Vdly21XsJiU991Cd@ds257241.mlab.com:57241/interlink-meetup';
 mongoose.connect(DB_URL, (err) => {
     if(err) {
         console.error('Mongo connection FAIL: ' + err)
